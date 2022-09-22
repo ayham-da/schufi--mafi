5433a60b2bd71a2396b4404d76b6f61bd9681c0c
ayham-da <ayham.alaaeddin@gmail.com>
2022-09-22 18:14:14 +0200

    gallery

--- src/pages/Gallery/Share3/Share3.js
+++ src/pages/Gallery/Share3/Share3.js
@@ -17,11 +17,11 @@ function Share3Page() {
         {
             img: share3_1,
             title: 'share3',
-            description: "Share3 ‚Ar : Street‘ ist ein Hip-Hop-Festival für aufstrebende Talente aus der Region Levant (Sham) im Nahen Osten.\n" +
+            description: "Share3 ‚Ar : Street‘ is a Hip-Hop festival dedicated for emerging talents from the Levant (Sham) area in the middle east.\n" +
                 "\n" +
-                "Auf den Straßen von Damaskus, Beirut und anderen Städten in der Levante erhoben Menschen ihre Stimme für Freiheit und Menschenrechte. In dieser Hinsicht wurden Rap- und Hip-Hop-Künstler von dieser Bewegung beeinflusst und versuchten, ihr Handwerk zusammenzubringen, um eine kulturelle Bewegung zu initiieren, die mit den Erfahrungen der Menschen und ihren Forderungen auf der Straße in Einklang steht.\n" +
+                "From the streets of Damascus, Beirut and other cities in the Levant, people raised their voices for freedom & human rights. In this respect, Rap & Hip-Hop artists got influenced by this movement and endeavored to put their craft together to initiate a cultural movement which resonates with the folks' experiences and their demands in the street.\n" +
                 "\n" +
-                "Share3 wird diesen Künstlern eine Plattform geben, um ihre Talente zu präsentieren, ihre Geschichten zu teilen und diese kulturelle Bewegung in der Levante sowie in Europa voranzutreiben, wo viele Menschen (einschließlich Künstler) aus der Levante einen neuen Ort gefunden haben, an dem sie sich frei ausdrücken können."
+                "Share3 will give these artists a platform to present their talents, share their stories and to push this cultural movement in the Levant as well in Europe where many people (including artists) from the Levant found a new place where they could express themselves freely."
         },
         {
             img: share3_2,
