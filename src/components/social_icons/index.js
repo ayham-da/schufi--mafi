3be297654b805d9b34812412945380e9d61ab602
ayham-da <ayham.alaaeddin@gmail.com>
2022-09-22 17:20:24 +0200

    edit icon

--- src/components/social_icons/index.js
+++ src/components/social_icons/index.js
@@ -4,19 +4,22 @@ import {
  Col,
 } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTiktok, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faTiktok, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 function SocialIcons() {
   return (
          <Col sm={12} className="social-icons">
           <a href="https://www.facebook.com/Schu.fi.ma.fi" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon className="fb" icon={faFacebook} size="2x"/>
+              <FontAwesomeIcon className="fb" icon={faFacebook} size="2x"/>
           </a>
           <a href="https://www.tiktok.com/@schufimafi.collective" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon className="tiktok" icon={faTiktok} size="2x"/>
+              <FontAwesomeIcon className="tiktok" icon={faTiktok} size="2x"/>
+          </a>
+          <a href="https://www.youtube.com/c/SchuFiMaFiKollektiv" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon className="youtube" icon={faYoutube} size="2x"/>
           </a>
           <a href="https://www.instagram.com/schufimaficollective/" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon className="insta" icon={faInstagram} size="2x"/>
+              <FontAwesomeIcon className="insta" icon={faInstagram} size="2x"/>
           </a>
          </Col>
   );
